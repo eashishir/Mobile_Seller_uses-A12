b670670bff6cccf6a5d8cfcbb1f5503591e0f7cb
shishir <eftekharAhmedshishir@gmail.com>
2022-11-30 22:11:02 +0600

    vercel link input

--- src/Pages/Home/ProductsCategories/ProductsCategories.js
+++ src/Pages/Home/ProductsCategories/ProductsCategories.js
@@ -23,16 +23,16 @@ const ProductsCategories = () => {
             <div>
                 <h2 className='text-center text-dark font-bold text-2xl'>Our Products <small className='text-2xl text-green-600'>Categories</small></h2>
 
-                <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-3'>
+                <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-3 '>
                     {
                         categories?.map(category => {
                             return (
-                                <div key={category._id} className="card w-96 bg-base-100 shadow-xl">
+                                <div key={category._id} className="card w-96 bg-black shadow-xl">
                                     <figure className="px-10 pt-10 ">
                                         <img src={category.logo} alt="Shoes" className="rounded-xl w-24" />
                                     </figure>
                                     <div className="card-body items-center text-center">
-                                        <h2 className="card-title font-bold">{category.name}</h2>
+                                        <h2 className="card-title text-white font-bold">{category.name}</h2>
 
                                         <div className="card-actions">
                                             <Link to={`/products/${category.categoryId}`}><button className="btn btn-outline btn-accent">Check Out</button></Link>
